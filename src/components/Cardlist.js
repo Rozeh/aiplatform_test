2d83664c99e24700a75947d48524c16e560efeae
rozeh <crozeh56@gmail.com>
2017-05-11 00:02:50 +0900

    카드리스트 추가

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -1,31 +1,43 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
-const CardList = () => (
-    <div className="taught-by-leaders__card">
-    <Card>
-        <Image src='/static/img/cardlist/1.jpg' />
-        <Card.Content>
-            <Card.Header>
-                Matthew
-            </Card.Header>
-            <Card.Meta>
+class Cardlist extends React.Component{
+    render(){
+        return(
+            <div className="taught-by-leaders__card">
+                <Card>
+                    <Image src={this.props.img} />
+                    <Card.Content>
+                        <Card.Header>
+                            {this.props.name}
+                        </Card.Header>
+                        <Card.Meta>
         <span className='date'>
-          Joined in 2015
+            {this.props.date}
         </span>
-            </Card.Meta>
-            <Card.Description>
-                Matthew is a musician living in Nashville.
-            </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-            <a>
-                <Icon name='user' />
-                22 Friends
-            </a>
-        </Card.Content>
-    </Card>
-    </div>
-)
+                        </Card.Meta>
+                        <Card.Description>
+                            {this.props.des}
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <a>
+                            <Icon name='user' />
+                            {this.props.cou}명
+                        </a>
+                    </Card.Content>
+                </Card>
+            </div>
+        );
+    }
+}
 
-export default CardList;
+
+Cardlist.defaultProps = {
+    img : "",
+    name : "",
+    date : "",
+    cou : 0
+}
+
+export default Cardlist;
--- src/container/Si2Main.js
+++ src/container/Si2Main.js
@@ -12,14 +12,13 @@ class Si2Main extends React.Component{
                 <div>
                 <div className="taught-by-leaders" >
                     <div className="taught-by-leaders__inner">
-                        <h2 className="taught-by-leaders__title">AI Platform의 교육리더들과 함께하세요.</h2>
+                        <h2 className="taught-by-leaders__title">디자인 강의 교육리더들과 함께하세요.</h2>
                         <h3 className="taught-by-leaders__subtitle">실무영역에서 적용되는 꼭 필요한 강좌를 들어보세요.</h3>
                         <div className="taught-by-leaders__cards-holder">
-                            <Cardlist/>
-                            <Cardlist/>
-                            <Cardlist/>
-
-
+                            <Cardlist img="/static/img/cardlist/2.png" name="포토샵" date="2017.05.06" des="블라블라블라" cou={5}></Cardlist>
+                            <Cardlist img="/static/img/cardlist/3.png" name="일러스트" date="2017.05.06" des="블라블라블라" cou={5}></Cardlist>
+                            <Cardlist img="/static/img/cardlist/4.jpg" name="인디자인" date="2017.05.06" des="블라블라블라" cou={5}></Cardlist>
+                       
                         </div>
                     </div>
                 </div>
